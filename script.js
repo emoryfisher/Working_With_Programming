a435578227c6e5237cb8f7e9f5006717bb02fd32
emoryfisher <70530177+emoryfisher@users.noreply.github.com>
2020-09-21 12:19:05 -0400

    Working with programming

---
+++ script.js
@@ -0,0 +1,12 @@
+var CalcEle = document.body.querySelector(".Calc");
+
+var userInput = prompt("What is your number?");
+
+var userInputTwo = prompt("What is your second number?"); 
+
+var numberOne = 6;
+var numberTwo = 3;
+
+CalcEle.innerHTML = numberOne+numberTwo;
+
+//CalcEle.innerHTML = userInput+userInputTwo;
